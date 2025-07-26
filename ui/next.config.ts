33e586e468fbebb8fa9a81c1e0d1ca986fa77165
bornjre <bornjre@gmail.com>
2025-07-27 03:13:21 +0545

    placeholder pages

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  basePath: "/z",
 };
 
 export default nextConfig;
