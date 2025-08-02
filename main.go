6b4854c575abb193e87db60efbf0dc95a5caaa8f
bornjre <bornjre@gmail.com>
2025-08-02 09:40:17 +0545

    stash

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	_ "github.com/blue-monads/turnix/backend/distro/climux/cplane"
 	_ "github.com/blue-monads/turnix/backend/distro/climux/node"
 	_ "github.com/mattn/go-sqlite3"
-
-	_ "github.com/blue-monads/turnix/backend/modules/books"
-	_ "github.com/blue-monads/turnix/backend/modules/unloop"
 )
 
 func main() {
