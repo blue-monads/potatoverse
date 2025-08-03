de168a8d38fa9181aa0fcf069cee4b86b978c4f4
bornjre <bornjre@gmail.com>
2025-08-03 10:49:08 +0545

    skeleton compo wip

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        data-theme="cerberus"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
