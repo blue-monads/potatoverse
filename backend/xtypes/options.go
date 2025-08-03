822311c60d2d660ea1f47182a8d07ebbb5eb5738
bornjre <bornjre@gmail.com>
2025-08-03 12:01:45 +0545

    nit

--- backend/xtypes/options.go
+++ backend/xtypes/options.go
@@ -1,10 +1,20 @@
 package xtypes
 
 type AppOptions struct {
-	Name         string `json:"name"`
-	Port         int    `json:"port"`
-	Host         string `json:"host"`
-	MasterSecret string `json:"master_secret"`
-	Debug        bool   `json:"debug"`
-	WorkingDir   string `json:"working_dir"`
+	Name         string        `json:"name"`
+	Port         int           `json:"port"`
+	Host         string        `json:"host"`
+	MasterSecret string        `json:"master_secret"`
+	Debug        bool          `json:"debug"`
+	WorkingDir   string        `json:"working_dir"`
+	Mailer       MailerOptions `json:"mailer"`
+}
+
+type MailerOptions struct {
+	Type     string            `json:"type"` // smtp, gmail, webhook
+	Host     string            `json:"host"`
+	Port     int               `json:"port"`
+	Username string            `json:"username"`
+	Password string            `json:"password"`
+	Meta     map[string]string `json:"meta"`
 }
