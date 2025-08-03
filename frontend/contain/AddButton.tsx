0f3ee212b8cbc011a3fd7506916bc5246a12761c
bornjre <bornjre@gmail.com>
2025-08-03 11:48:49 +0545

    use theme  style

--- frontend/contain/AddButton.tsx
+++ frontend/contain/AddButton.tsx
@@ -11,7 +11,7 @@ export const AddButton: React.FC<PropsType> = (props: PropsType) => {
         <button
             onClick={props.onClick}
             disabled={props.disabled}
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg font-medium hover:bg-blue-700 transition-colors"
+            className="btn btn-base preset-filled"
             >
             {props.name || "Add"}
         </button>
