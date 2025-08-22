cf2ea67dfdfe62b61f43190b127c8ccb14c1940c
bornjre <bornjre@gmail.com>
2025-08-23 03:36:29 +0545

    stash

--- frontend/app/play/page.tsx
+++ frontend/app/play/page.tsx
@@ -1,31 +1,73 @@
 "use client";
 
-import React, { useState } from 'react';
-import { Tabs } from '@skeletonlabs/skeleton-react';
+import React, { useState } from "react";
+import { Tabs } from "@skeletonlabs/skeleton-react";
+import FantasticTable from "@/contain/FantasticTable/FantasticTable";
 
 const Page: React.FC = () => {
-  const [group, setGroup] = useState('plane');
+  const [group, setGroup] = useState("plane");
   const lorem =
-    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum veniam reprehenderit eum, reiciendis obcaecati, excepturi nemo ipsa fugit suscipit autem vitae numquam et cumque praesentium vero eos minus itaque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum veniam reprehenderit eum, reiciendis obcaecati, excepturi nemo.';
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum veniam reprehenderit eum, reiciendis obcaecati, excepturi nemo ipsa fugit suscipit autem vitae numquam et cumque praesentium vero eos minus itaque. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum veniam reprehenderit eum, reiciendis obcaecati, excepturi nemo.";
 
   return (
-    <Tabs value={group} onValueChange={(e) => setGroup(e.value!)}>
-      <Tabs.List>
-        <Tabs.Control value="plane">Plane</Tabs.Control>
-        <Tabs.Control value="boat">Boat</Tabs.Control>
-        <Tabs.Control value="car">Car</Tabs.Control>
-      </Tabs.List>
-      <Tabs.Content>
-        <Tabs.Panel value="plane">Plane Panel - {lorem}</Tabs.Panel>
-        <Tabs.Panel value="boat">Boat Panel - {lorem}</Tabs.Panel>
-        <Tabs.Panel value="car">Car Panel - {lorem}</Tabs.Panel>
-      </Tabs.Content>
-    </Tabs>
+    // <Tabs value={group} onValueChange={(e) => setGroup(e.value!)}>
+    //   <Tabs.List>
+    //     <Tabs.Control value="plane">Plane</Tabs.Control>
+    //     <Tabs.Control value="boat">Boat</Tabs.Control>
+    //     <Tabs.Control value="car">Car</Tabs.Control>
+    //   </Tabs.List>
+    //   <Tabs.Content>
+    //     <Tabs.Panel value="plane">Plane Panel - {lorem}</Tabs.Panel>
+    //     <Tabs.Panel value="boat">Boat Panel - {lorem}</Tabs.Panel>
+    //     <Tabs.Panel value="car">Car Panel - {lorem}</Tabs.Panel>
+    //   </Tabs.Content>
+    // </Tabs>
+
+    <>
+      <div className="p-4">
+        <FantasticTable
+          columns={[
+            { title: "Position", key: "position" },
+            { title: "Symbol", key: "symbol" },
+            { title: "Name", key: "name" },
+            {
+              title: "Atomic No",
+              key: "atomic_no",
+              render: (data) => <span className="text-right">{data}</span>,
+            },
+          ]}
+          data={[
+            { position: 1, symbol: "H", name: "Hydrogen", atomic_no: 1 },
+            { position: 2, symbol: "He", name: "Helium", atomic_no: 2 },
+            { position: 3, symbol: "Li", name: "Lithium", atomic_no: 3 },
+            { position: 4, symbol: "Be", name: "Beryllium", atomic_no: 4 },
+            { position: 5, symbol: "B", name: "Boron", atomic_no: 5 },
+            { position: 6, symbol: "C", name: "Carbon", atomic_no: 6 },
+            { position: 7, symbol: "N", name: "Nitrogen", atomic_no: 7 },
+            { position: 8, symbol: "O", name: "Oxygen", atomic_no: 8 },
+            { position: 9, symbol: "F", name: "Fluorine", atomic_no: 9 },
+            { position: 10, symbol: "Ne", name: "Neon", atomic_no: 10 },
+          ]}
+          captionText="Periodic Table - First 10 Elements"
+          isLoading={false}
+          onRowClick={(row) => alert(`You clicked on ${row.name}`)}
+          noDataMessage="No elements found."
+          actions={[
+            {
+              label: "Add Element",
+              onClick: () => alert("Add Element Clicked"),
+              dropdown: false,
+            },
+            {
+              label: "Export",
+              onClick: () => alert("Export Clicked"),
+              dropdown: true,
+            },
+          ]}
+        />
+      </div>
+    </>
   );
 };
-
-
-
-
 
 export default Page;
---
+++ frontend/contain/FantasticTable/FantasticTable.tsx
@@ -0,0 +1,219 @@
+import { useEffect, useRef, useState } from "react";
+import { DotSquare, Search, Zap, EllipsisVertical } from "lucide-react";
+
+interface PropsType {
+  columns: ColumnDef[];
+  data: any[];
+  isLoading?: boolean;
+  error?: string;
+  noDataMessage?: string;
+  onRowClick?: (rowData: any) => void;
+  captionText?: string;
+  actions?: Actions[];
+}
+
+interface ColumnDef {
+  title: string;
+  key: string;
+  render?: (data: any) => React.ReactNode;
+  searchable?: boolean;
+}
+
+interface Actions {
+  label: string;
+  onClick: (rowData: any) => void;
+  icon?: React.ReactNode;
+  dropdown?: boolean;
+  className?: string;
+}
+
+const FantasticTable = (props: PropsType) => {
+  const dropDownActions = props.actions
+    ? props.actions.filter((action) => action.dropdown)
+    : [];
+  const notDropDownActions = props.actions
+    ? props.actions.filter((action) => !action.dropdown)
+    : [];
+
+  return (
+    <div className="table-wrap">
+      <table className="table caption-bottom">
+        <caption className="pt-4">{props.captionText}</caption>
+        <thead>
+          <tr>
+            {props.columns.map((col) => (
+              <th key={col.key}>{col.title}</th>
+            ))}
+
+            {props.actions && (
+              <>
+                <td>Actions</td>
+              </>
+            )}
+          </tr>
+        </thead>
+
+        <tbody className="[&>tr]:hover:preset-tonal-primary">
+          {props.isLoading ? (
+            <tr>
+              <td colSpan={props.columns.length} className="text-center py-4">
+                Loading...
+              </td>
+            </tr>
+          ) : props.error ? (
+            <tr>
+              <td
+                colSpan={props.columns.length}
+                className="text-center py-4 text-red-500"
+              >
+                {props.error}
+              </td>
+            </tr>
+          ) : props.data.length === 0 ? (
+            <tr>
+              <td colSpan={props.columns.length} className="text-center py-4">
+                {props.noDataMessage || "No data available."}
+              </td>
+            </tr>
+          ) : (
+            props.data.map((row, rowIndex) => {
+                const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+                const [buttonRect, setButtonRect] = useState<DOMRect | null>(null);
+                const buttonRef = useRef<HTMLButtonElement>(null);
+
+                    const handleToggleDropdown = () => {
+                            if (!isDropdownOpen && buttonRef.current) {
+                                const rect = buttonRef.current.getBoundingClientRect();
+                                setButtonRect(rect);
+                            }
+                            setIsDropdownOpen(!isDropdownOpen);
+                    };
+
+
+                useEffect(() => {
+                    const handleClickOutside = (event: MouseEvent) => {
+                        if (isDropdownOpen && buttonRef.current && !buttonRef.current.contains(event.target as Node)) {
+                            setIsDropdownOpen(false);
+                        }
+                    };
+
+                    const handleScroll = () => {
+                        if (isDropdownOpen && buttonRef.current) {
+                            const rect = buttonRef.current.getBoundingClientRect();
+                            setButtonRect(rect);
+                        }
+                    };
+
+                    document.addEventListener('mousedown', handleClickOutside);
+                    window.addEventListener('scroll', handleScroll, true);
+                    window.addEventListener('resize', handleScroll);
+
+                    return () => {
+                        document.removeEventListener('mousedown', handleClickOutside);
+                        window.removeEventListener('scroll', handleScroll, true);
+                        window.removeEventListener('resize', handleScroll);
+                    };
+                }, [isDropdownOpen]);
+
+              return (
+                <tr
+                  key={rowIndex}
+                  onClick={() => props.onRowClick && props.onRowClick(row)}
+                  className={props.onRowClick ? "cursor-pointer" : ""}
+                >
+                  {props.columns.map((col) => (
+                    <td key={col.key}>
+                      {col.render ? col.render(row[col.key]) : row[col.key]}
+                    </td>
+                  ))}
+
+                  {props.actions && (
+                    <td className="text-right">
+                      {notDropDownActions.map((action, actionIndex) => (
+                        <button
+                          key={actionIndex}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            action.onClick(row);
+                          }}
+                          className={`btn btn-sm btn-base preset-filled ${action.className || ""}`}
+                        >
+                          {action.icon && (
+                            <span className="mr-1">{action.icon}</span>
+                          )}
+                          {action.label}
+                        </button>
+                      ))}
+
+                      {dropDownActions.length > 0 && (
+                        <div className="relative inline-block text-left">
+                          <div>
+                            <button
+                              type="button"
+                              className="inline-flex"
+                              id="menu-button"
+                              aria-expanded="true"
+                              aria-haspopup="true"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleToggleDropdown();
+                              }}
+                              ref={buttonRef}
+                            >
+                              <EllipsisVertical className="h-5 w-5" />
+                            </button>
+                          </div>
+                          
+                          {isDropdownOpen && buttonRect && (
+                            <div
+                              className="origin-top-right fixed right-0 mt-2 w-48  bg-white shadow-lg "
+                              style={{
+                                top: buttonRect.bottom + 4,
+                                left: buttonRect.right - 200,
+                              }}
+                              role="menu"
+                              aria-orientation="vertical"
+                              aria-labelledby="menu-button"
+                              tabIndex={-1}
+                            >
+                              {dropDownActions.map((action, actionIndex) => (
+                                <button
+                                  key={actionIndex}
+                                  onClick={(e) => {
+                                    e.stopPropagation();
+                                    action.onClick(row);
+                                    setIsDropdownOpen(false);
+                                  }}
+                                  className={`w-full text-left px-3 py-2 text-sm first:rounded-t-lg last:rounded-b-lg text-gray-700 hover:text-blue-600 transition-colors hover:bg-gray-200 cursor-pointer ${action.className || ""}`}
+                                  role="menuitem"
+                                  tabIndex={-1}
+                                  id={`menu-item-${actionIndex}`}
+                                >
+                                  <div className="inline-flex items-center gap-2">
+                                    {action.icon}
+                                    {action.label}
+                                  </div>
+                                </button>
+                              ))}
+                            </div>
+                          )}
+                        </div>
+                        
+                      )}
+                    </td>
+                  )}
+                </tr>
+              );
+            })
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+
+
+
+
+export default FantasticTable;
