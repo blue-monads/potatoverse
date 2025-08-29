5240dd85ade693484527ae8f3e8421341595eaa7
bornjre <bornjre@gmail.com>
2025-08-30 03:08:56 +0545

    fix next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: "/z/pages",
-  distDir: "output/build",
+  output: "export",
+  distDir: isProduction ? "output/build" : ".next",
   images: {
     unoptimized: true,
   },
