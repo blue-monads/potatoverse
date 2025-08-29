fc1b80f9197a6af5d2664595e519ddde33cf0f03
bornjre <bornjre@gmail.com>
2025-08-30 02:08:18 +0545

    stash

--- backend/app/actions/auth.go
+++ backend/app/actions/auth.go
@@ -1,1 +1,52 @@
-package controller
+package actions
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+
+	"github.com/blue-monads/turnix/backend/services/datahub/models"
+	"github.com/blue-monads/turnix/backend/services/signer"
+	"github.com/rs/xid"
+)
+
+type LoginOpts struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var phoneRegex = regexp.MustCompile(`^\+?[1-9]\d{1,14}$`)
+
+func (c *Controller) Login(opts LoginOpts) (string, error) {
+
+	var user *models.User
+
+	if strings.Contains(opts.Email, "@") {
+		_user, err := c.database.GetUserByEmail(opts.Email)
+		if err != nil {
+			return "", err
+		}
+		user = _user
+	} else if phoneRegex.MatchString(opts.Email) {
+		return "", errors.New("implement login by phone")
+	} else {
+		return "", errors.New("implement login by username")
+	}
+
+	// fixme => hash it
+	if user.Password != opts.Password {
+		return "", errors.New("invalid password")
+	}
+
+	token, err := c.signer.SignAccess(&signer.AccessClaim{
+		XID:    xid.New().String(),
+		UserId: int64(user.ID),
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+
+}
--- backend/app/server/auth.go
+++ backend/app/server/auth.go
@@ -1,58 +1,31 @@
 package server
 
 import (
-	"regexp"
+	"net/http"
 
+	"github.com/blue-monads/turnix/backend/app/actions"
+	"github.com/blue-monads/turnix/backend/utils/libx/httpx"
 	"github.com/gin-gonic/gin"
 )
 
-type loginDetails struct {
-	Email    string `db:"email"`
-	Password string `db:"password"`
-}
-
-var phoneRegex = regexp.MustCompile(`^\+?[1-9]\d{1,14}$`)
-
 func (a *Server) login(ctx *gin.Context) {
-	// data := &loginDetails{}
-	// err := ctx.Bind(data)
-	// if err != nil {
-	// 	httpx.WriteAuthErr(ctx, err)
-	// 	return
-	// }
+	data := &actions.LoginOpts{}
 
-	// var user *models.User
+	// LoginOpts
+	err := ctx.Bind(data)
+	if err != nil {
+		httpx.WriteAuthErr(ctx, err)
+		return
+	}
 
-	// if strings.Contains(data.Email, "@") {
-	// 	user, err = a.db.GetUserByEmail(data.Email)
-	// 	if err != nil {
-	// 		httpx.WriteAuthErr(ctx, err)
-	// 		return
-	// 	}
-	// } else if phoneRegex.MatchString(data.Email) {
-	// 	panic("Implement login by phone")
-	// } else {
-	// 	panic("Implement login by username")
-	// }
+	token, err := a.ctrl.Login(*data)
+	if err != nil {
+		httpx.WriteAuthErr(ctx, err)
+		return
+	}
 
-	// // fixme => hash it
-	// if user.Password != data.Password {
-	// 	httpx.WriteAuthErr(ctx, err)
-	// 	return
-	// }
-
-	// token, err := a.signer.SignAccess(&signer.AccessClaim{
-	// 	XID:    xid.New().String(),
-	// 	UserId: int64(user.ID),
-	// })
-
-	// if err != nil {
-	// 	httpx.WriteErr(ctx, err)
-	// 	return
-	// }
-
-	// ctx.JSON(http.StatusOK, gin.H{
-	// 	"access_token": token,
-	// })
+	ctx.JSON(http.StatusOK, gin.H{
+		"access_token": token,
+	})
 
 }
