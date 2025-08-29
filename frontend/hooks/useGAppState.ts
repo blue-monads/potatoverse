48b8dc0617b7412f57936e537b6f059c5c387f96
bornjre <bornjre@gmail.com>
2025-08-30 03:51:24 +0545

    global hook placeholder

---
+++ frontend/hooks/useGAppState.ts
@@ -0,0 +1,32 @@
+import { getAccessToken } from "@/lib/api";
+import { useEffect, useState } from "react";
+
+
+export const useGAppState = () => {    
+    const [isLoading, setIsLoading] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const checkToken = () => {
+        const accessToken = getAccessToken();
+        if (accessToken) {
+            setIsAuthenticated(true);
+        } else {
+            setIsAuthenticated(false);
+        }        
+        setIsLoading(false);        
+    }
+
+
+    useEffect(() => {
+        checkToken();
+    }, []);
+
+
+    return {
+        isLoading,
+        isAuthenticated,
+        checkToken,
+    }
+}
+
+export type Handle = ReturnType<typeof useGAppState>;
