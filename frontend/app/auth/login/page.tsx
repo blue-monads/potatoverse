d68949dbe3878a98aa8565fa6ae206dab327bc31
bornjre <bornjre@gmail.com>
2025-08-31 00:21:26 +0545

    nit

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
             saveAccessToken(token);
             initHttpClient();
 
-            router.push("/admin");
+            router.push("/portal/admin");
 
 
 
