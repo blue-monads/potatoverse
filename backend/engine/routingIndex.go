40525d17d04050c29a0c66964657fe0ea4c1903b
bornjre <bornjre@gmail.com>
2025-10-10 23:53:34 +0545

    nit

--- backend/engine/routingIndex.go
+++ backend/engine/routingIndex.go
@@ -1,7 +1,17 @@
 package engine
 
 type SpaceRouteIndexItem struct {
-	packageId   int64
-	spaceId     int64
-	serveFolder string
+	packageId         int64
+	spaceId           int64
+	overlayForSpaceId int64
+	serveFolder       string
+	trimPathPrefix    string
+}
+
+type PluginRouteIndexItem struct {
+	pluginId       int64
+	packageId      int64
+	spaceId        int64
+	trimPathPrefix string
+	serveFolder    string
 }
