ee5fd26efc85d124a96df77e8818db84ff35ba46
bornjre <bornjre@gmail.com>
2025-10-11 17:47:36 +0545

    nit

--- ci.sh
+++ ci.sh
@@ -5,17 +5,8 @@ set -e
 
 echo "Running citest"
 
-RUNUNITTESTS=0
 RUNINTEGRATIONTESTS=1
 
-
-if [ "$RUNUNITTESTS" -eq 1 ]; then
-  echo "Running unit tests"
-  go test -v -timeout 30s -run ^TestAutoCapture$ ./backend/extras/autocdc
-  go test -v -timeout 30s -run ^TestBinds$ ./backend/engine/luaz/binds
-else
-  echo "Skipping unit tests"
-fi
 
 if [ "$RUNINTEGRATIONTESTS" -eq 1 ]; then
   echo "Running integration tests"
