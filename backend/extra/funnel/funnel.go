44c4ad2d1d1b5ad185d1d560abb76da78a0e2464
bornjre <bornjre@gmail.com>
2025-11-04 23:52:54 +0545

    nit

--- backend/extra/funnel/funnel.go
+++ backend/extra/funnel/funnel.go
@@ -3,6 +3,8 @@ package funnel
 import (
 	"net"
 	"sync"
+
+	"github.com/gin-gonic/gin"
 )
 
 // root_<pub_key_hash>.freehttptunnel.com
@@ -15,4 +17,13 @@ type ServerConnection struct {
 type Funnel struct {
 	ServerConnections map[string]*ServerConnection
 	scLock            sync.RWMutex
+}
+
+func (f *Funnel) HandleVerify(c *gin.Context) {}
+
+func (f *Funnel) HandleConnect(c *gin.Context) {}
+
+func SignWithPubKey(pubKey, signPayload string) (string, error) {
+
+	return "", nil
 }
