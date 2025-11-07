fa39c9efb52f47bd0384b51948343bcc888a0cca
bornjre <bornjre@gmail.com>
2025-11-08 02:57:07 +0545

    refactor packaging

--- cmd/cli/package.go
+++ cmd/cli/package.go
@@ -2,85 +2,15 @@ package cli
 
 import (
 	"archive/zip"
-	"encoding/json"
 	"fmt"
 	"os"
-	"path"
 	"path/filepath"
 	"regexp"
 	"strings"
 
-	"github.com/alecthomas/kong"
 	"github.com/blue-monads/turnix/backend/xtypes/models"
 	"github.com/pelletier/go-toml/v2"
 )
-
-func (c *PackageBuildCmd) Run(_ *kong.Context) error {
-	_, err := PackageFiles(c.PotatoTomlFile, c.OutputZipFile)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// simple.chip.zip
-// simple.czip
-
-func PackageFiles(potatoTomlFile string, outputZipFile string) (string, error) {
-	potatoToml, err := readPotatoToml(potatoTomlFile)
-	if err != nil {
-		return "", err
-	}
-
-	if outputZipFile == "" {
-		outputZipFile = potatoToml.Packaging.OutputZipFile
-		if outputZipFile == "" {
-			outputZipFile = fmt.Sprintf("%s.zip", potatoToml.Slug)
-		}
-	}
-
-	zipFile, err := os.Create(outputZipFile)
-	if err != nil {
-		return "", err
-	}
-	defer zipFile.Close()
-
-	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
-
-	potatoFileDir := path.Dir(potatoTomlFile)
-
-	err = packageFilesV2(potatoFileDir, potatoToml.Packaging, zipWriter)
-	if err != nil {
-		return "", err
-	}
-
-	potatoToml.DevToken = ""
-
-	potatoJson, err := json.Marshal(potatoToml)
-	if err != nil {
-		return "", err
-	}
-
-	pfile, err := zipWriter.Create("potato.json")
-	if err != nil {
-		return "", err
-	}
-	_, err = pfile.Write(potatoJson)
-	if err != nil {
-		return "", err
-	}
-
-	err = zipWriter.Close()
-	if err != nil {
-		return "", err
-	}
-
-	fmt.Printf("Package built successfully: %s\n", outputZipFile)
-
-	return outputZipFile, nil
-}
 
 func readPotatoToml(potatoTomlFile string) (*models.PotatoPackage, error) {
 	potatoTomlFileData, err := os.ReadFile(potatoTomlFile)
@@ -367,5 +297,3 @@ func globToRegex(pattern string) (*regexp.Regexp, error) {
 	regexPattern := "^" + escaped + "$"
 	return regexp.Compile(regexPattern)
 }
-
-// ssd
---
+++ cmd/cli/package_push.go
@@ -0,0 +1,9 @@
+package cli
+
+import (
+	"github.com/alecthomas/kong"
+)
+
+func (c *PackagePushCmd) Run(_ *kong.Context) error {
+	panic("Package Push - Not implemented yet")
+}
