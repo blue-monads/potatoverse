5dd3a21048fac4482d25f9d290af9f8bb9b24ddb
bornjre <bornjre@gmail.com>
2025-11-08 07:10:17 +0545

    push only command

--- cmd/cli/package.go
+++ cmd/cli/package.go
@@ -1,8 +1,17 @@
 package cli
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/alecthomas/kong"
+)
+
 type PackageCmd struct {
-	Build PackageBuildCmd `cmd:"" help:"Build the package."`
-	Push  PackagePushCmd  `cmd:"" help:"Push the package."`
+	Build    PackageBuildCmd `cmd:"" help:"Build the package."`
+	Push     PackagePushCmd  `cmd:"" help:"Push the package."`
+	PushOnly PackagePushOnly `cmd:"" help:"Push the package only."`
 }
 
 type PackagePushCmd struct {
@@ -13,4 +22,47 @@ type PackagePushCmd struct {
 type PackageBuildCmd struct {
 	PotatoTomlFile string `name:"potato-toml-file" help:"Path to package directory." type:"path" default:"./potato.toml"`
 	OutputZipFile  string `name:"output-zip-file" help:"Output path for built package." type:"path"`
+}
+
+type PackagePushOnly struct {
+	PotatoTomlFile string `name:"potato-toml-file" help:"Path to package directory." type:"path" default:"./potato.toml"`
+	OutputZipFile  string `name:"output-zip-file" help:"Output path for built package." type:"path"`
+}
+
+func (c *PackagePushOnly) Run(ctx *kong.Context) error {
+
+	return PushPackage(c.PotatoTomlFile, c.OutputZipFile)
+}
+
+func PushPackage(potatoTomlFile string, outputZipFile string) error {
+	potatoToml, err := readPotatoToml(potatoTomlFile)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Open(outputZipFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	req, err := http.NewRequest("POST", potatoToml.Developer.ServerUrl, file)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", potatoToml.Developer.Token)
+	req.Header.Set("Content-Type", "application/zip")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to push package: %s", resp.Status)
+	}
+
+	return nil
 }
--- cmd/cli/package_push.go
+++ cmd/cli/package_push.go
@@ -1,57 +1,15 @@
 package cli
 
 import (
-	"errors"
-	"net/http"
-	"os"
-
 	"github.com/alecthomas/kong"
 )
 
 func (c *PackagePushCmd) Run(_ *kong.Context) error {
-	potatoToml, err := readPotatoToml(c.PotatoTomlFile)
-	if err != nil {
-		return err
-	}
 
 	zip, err := PackageFiles(c.PotatoTomlFile, c.OutputZipFile)
 	if err != nil {
 		return err
 	}
 
-	token := potatoToml.Developer.Token
-	if token == "" {
-		return errors.New("token is required")
-	}
-
-	serverUrl := potatoToml.Developer.ServerUrl
-	if serverUrl == "" {
-		return errors.New("server url is required")
-	}
-
-	file, err := os.Open(zip)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	req, err := http.NewRequest("POST", serverUrl, file)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Authorization", token)
-	req.Header.Set("Content-Type", "application/zip")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return errors.New("failed to push package")
-	}
-
-	return nil
+	return PushPackage(c.PotatoTomlFile, zip)
 }
