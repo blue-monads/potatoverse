5015093038ea3efd1954ba262978dd52f0083202
bornjre <bornjre@gmail.com>
2025-11-09 14:28:53 +0545

    nit

--- backend/engine/executors/luaz/binds/b_core.go
+++ backend/engine/executors/luaz/binds/b_core.go
@@ -5,6 +5,9 @@ import (
 	lua "github.com/yuin/gopher-lua"
 )
 
+// sign capability token
+// sign fs file presigned token
+
 func CoreModule(app xtypes.App, installId int64, spaceId int64) func(L *lua.LState) int {
 	return func(L *lua.LState) int {
 		return 0
