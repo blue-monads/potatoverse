9fcbd083f8bace23943761193c20f6de12c749c3
bornjre <bornjre@gmail.com>
2025-11-11 01:50:34 +0545

    stash

--- backend/engine/executors/luaz/binds/b_core.go
+++ backend/engine/executors/luaz/binds/b_core.go
@@ -7,6 +7,18 @@ import (
 
 // sign capability token
 // sign fs file presigned token
+// presign package file presigned token
+// package_get_file
+// package_list_folder
+// write_temporary_file
+// read_temporary_file
+// delete_temporary_file
+// create_temporary_folder
+// delete_temporary_folder
+// list_temporary_folder
+// read_seek_temporary_file
+// write_seek_temporary_file
+// get_temporary_file_info
 
 func CoreModule(app xtypes.App, installId int64, spaceId int64) func(L *lua.LState) int {
 	return func(L *lua.LState) int {
