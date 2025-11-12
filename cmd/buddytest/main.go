8e830946a6caf7b684acbf6985a434959096ffa7
bornjre <bornjre@gmail.com>
2025-11-12 20:45:54 +0545

    stash

--- cmd/buddytest/main.go
+++ cmd/buddytest/main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"log"
-	"time"
 
 	buddyfs "github.com/blue-monads/turnix/backend/labs/buddyFs"
 	"github.com/gin-gonic/gin"
 )
 
-func Mmain1() {
-	RunFtpServer()
-
-}
-
 func main() {
-	WebdavServer()
+
 }
 
 func Mmain2() {
@@ -28,43 +22,6 @@ func Mmain2() {
 
 	buddyFs.Mount(engine.Group("/buddyfs"))
 
-	go BetterFtpServer()
-
 	engine.Run(":8666")
-
-}
-
-func buddyClientRun() {
-
-	time.Sleep(2 * time.Second)
-
-	client := buddyfs.NewBuddyFsClient("http://localhost:8666/buddyfs")
-
-	err := client.Ping()
-	if err != nil {
-		log.Printf("Failed to ping: %v", err)
-		return
-	}
-
-	file, err := client.Create("test.txt")
-	if err != nil {
-		log.Printf("Failed to create file: %v", err)
-		return
-	}
-
-	defer file.Close()
-
-	_, err = file.WriteString("Hello, World!")
-	if err != nil {
-		log.Printf("Failed to write to file: %v", err)
-		return
-	}
-
-	file, err = client.Open("test.txt")
-	if err != nil {
-		log.Printf("Failed to open file: %v", err)
-		return
-	}
-	defer file.Close()
 
 }
