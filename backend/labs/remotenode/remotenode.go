fc71009e67ecb7a23fe7ddee31dcac3882766c11
bornjre <bornjre@gmail.com>
2025-11-25 16:53:02 +0545

    nit

--- backend/labs/remotenode/remotenode.go
+++ backend/labs/remotenode/remotenode.go
@@ -3,3 +3,15 @@ package eventhub
 type RemoteNode interface {
 	Start() error
 }
+
+/*
+
+/zz/remote/sysevent
+/zz/remote/dbhub/
+/zz/remote/engine/
+
+*/
+
+type SystemEventHub struct {
+	nodes map[string]RemoteNode
+}
