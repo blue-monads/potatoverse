271f3214247069dd466a1ef6b227216e5b78e700
bornjre <bornjre@gmail.com>
2025-11-25 18:18:30 +0545

    nit

--- backend/labs/remotenode/remotenode.go
+++ backend/labs/remotenode/remotenode.go
@@ -10,6 +10,12 @@ type RemoteNode interface {
 /zz/remote/dbhub/
 /zz/remote/engine/
 
+exec stretegy ->
+- always_remote
+- pefer_remote
+- always_local
+- perfer_local
+
 */
 
 type SystemEventHub struct {
