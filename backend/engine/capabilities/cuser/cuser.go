de12a7b595bc13d558343d93181c0bf0485135ca
bornjre <bornjre@gmail.com>
2025-11-28 20:23:37 +0545

    stash user capability actions

--- backend/engine/capabilities/cuser/cuser.go
+++ backend/engine/capabilities/cuser/cuser.go
@@ -1,1 +1,15 @@
 package cuser
+
+/*
+
+query_user({offset: 0, limit: 100, group: ""})
+get_user_info(id: 1)
+query_user_config({user_id: 1, config_group: "TEST", limit: 100, offset: 0})
+update_user_config({user_id: 1, config_group: "TEST", config_key: "TEST", config_value: "TEST"})
+delete_user_config({user_id: 1, config_group: "TEST", config_key: "TEST"})
+send_user_message({user_id: 1, title: "TEST", message: "TEST"})
+
+
+
+
+*/
