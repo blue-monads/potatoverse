343a22dabc630f5c6f5ea3078a3a9f1ce03cb0f0
bornjre <bornjre@gmail.com>
2025-12-27 02:45:06 +0545

    nit

--- backend/engine/hubs/eventhub/reload.go
+++ backend/engine/hubs/eventhub/reload.go
@@ -1,18 +1,20 @@
 package eventhub
 
-import "github.com/blue-monads/turnix/backend/utils/qq"
+import (
+	"github.com/blue-monads/turnix/backend/utils/qq"
+)
 
 func (e *EventHub) watchReload() {
 
-	for {
-		select {
-		case <-e.refreshFullIndex:
-			qq.Println("@watchReload/refreshFullIndex")
-			err := e.buildActiveEventsIndex()
-			if err != nil {
-				qq.Println("@watchReload/2")
-			}
+	for event := range e.refreshFullIndex {
+
+		qq.Println("@watchReload/refreshFullIndex", event)
+
+		err := e.buildActiveEventsIndex()
+		if err != nil {
+			qq.Println("@watchReload/2", err)
 		}
+
 	}
 
 }
