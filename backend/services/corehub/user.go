1c9b0a7f4053fe974e7dd5a250dc82c0d984dfab
bornjre <bornjre@gmail.com>
2025-12-29 03:33:26 +0545

    user corehub

--- backend/services/corehub/user.go
+++ backend/services/corehub/user.go
@@ -1,6 +1,72 @@
 package corehub
 
-func (c *CoreHub) UserSendMessage(userId int64, message string) error {
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/blue-monads/turnix/backend/services/datahub/dbmodels"
+	"github.com/blue-monads/turnix/backend/utils/libx/httpx"
+	"github.com/blue-monads/turnix/backend/utils/qq"
+	"github.com/gin-gonic/gin"
+	"github.com/gobwas/ws"
+)
+
+func (c *CoreHub) UserSendMessage(msg *dbmodels.UserMessage) error {
+	notifier := c.sockd.GetNotifier()
+
+	now := time.Now()
+	msg.CreatedAt = &now
+
+	id, err := c.db.GetUserOps().AddUserMessage(msg)
+	if err != nil {
+		return err
+	}
+	msg.ID = id
+
+	jsonMsg, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+
+	err = notifier.SendUser(msg.ToUser, jsonMsg)
+	if err != nil {
+		return err
+	}
 
 	return nil
+}
+
+func (c *CoreHub) HandleUserWS(userId int64, ctx *gin.Context) {
+	dbUser, err := c.db.GetUserOps().GetUser(userId)
+	if err != nil {
+		httpx.WriteErrString(ctx, "failed to get user")
+		return
+	}
+
+	if dbUser.IsDeleted {
+		httpx.WriteErrString(ctx, "user is deleted")
+		return
+	}
+
+	if !dbUser.IsVerified {
+		httpx.WriteErrString(ctx, "user is not verified")
+		return
+	}
+
+	conn, _, _, err := ws.UpgradeHTTP(ctx.Request, ctx.Writer)
+	if err != nil {
+		httpx.WriteErrString(ctx, "failed to upgrade websocket")
+		return
+	}
+
+	ugroup := dbUser.Ugroup
+
+	connId, err := c.sockd.GetNotifier().AddUserConnection(userId, ugroup, conn)
+	if err != nil {
+		httpx.WriteErrString(ctx, "failed to add user connection")
+		return
+	}
+
+	qq.Println("@HandleUserWS/user connected", userId, connId)
+
 }
