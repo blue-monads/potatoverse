84672dc1cc16e75e89d40f291e28de2e0036114b
bornjre <bornjre@gmail.com>
2026-01-04 13:20:58 +0545

    refactor registry

--- backend/registry/caps.go
+++ backend/registry/caps.go
@@ -1,27 +1,13 @@
 package registry
 
 import (
-	"maps"
-	"sync"
-
 	"github.com/blue-monads/turnix/backend/xtypes/xcapability"
 )
 
-var (
-	CapabilityBuilderFactories = make(map[string]xcapability.CapabilityBuilderFactory)
-	mLock                      = sync.RWMutex{}
-)
-
 func RegisterCapability(name string, factory xcapability.CapabilityBuilderFactory) {
-	CapabilityBuilderFactories[name] = factory
+	registryStore.RegisterCapabilityBuilderFactory(name, factory)
 }
 
 func GetCapabilityBuilderFactories() (map[string]xcapability.CapabilityBuilderFactory, error) {
-	mLock.RLock()
-	defer mLock.RUnlock()
-
-	copy := make(map[string]xcapability.CapabilityBuilderFactory)
-	maps.Copy(copy, CapabilityBuilderFactories)
-
-	return copy, nil
+	return registryStore.GetCapabilityBuilderFactories(), nil
 }
--- backend/registry/executors.go
+++ backend/registry/executors.go
@@ -1,30 +1,13 @@
 package registry
 
 import (
-	"maps"
-	"sync"
-
 	"github.com/blue-monads/turnix/backend/xtypes"
 )
 
-var (
-	executorBuilderFactories = make(map[string]xtypes.ExecutorBuilderFactory)
-	meLock                   = sync.Mutex{}
-)
-
 func RegisterExecutorBuilderFactory(name string, factory xtypes.ExecutorBuilderFactory) {
-	meLock.Lock()
-	defer meLock.Unlock()
-
-	executorBuilderFactories[name] = factory
+	registryStore.RegisterExecutorBuilderFactory(name, factory)
 }
 
 func GetExecutorBuilderFactories() map[string]xtypes.ExecutorBuilderFactory {
-	meLock.Lock()
-	defer meLock.Unlock()
-
-	copy := make(map[string]xtypes.ExecutorBuilderFactory)
-	maps.Copy(copy, executorBuilderFactories)
-
-	return copy
+	return registryStore.GetExecutorBuilderFactories()
 }
