ed598f66c8fcc8248347832b5cc32252ee700b2a
bornjre <bornjre@gmail.com>
2026-01-17 21:47:43 +0545

    generated code

--- frontend/output/build/standalone/contain/compo/FantasticTable/ftypes.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Actions {
-    label: string;
-    onClick: (rowData: any) => void;
-    icon?: React.ReactNode | string;
-    dropdown?: boolean;
-    className?: string;
-    color?: "primary" | "secondary" | "success" | "danger" | "warning"
-  }
