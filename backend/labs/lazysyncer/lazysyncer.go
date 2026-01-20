2a69038cac1355a58eb52b0cc3c9b0c734310797
bornjre <bornjre@gmail.com>
2026-01-20 05:54:53 +0545

    nit

--- backend/labs/lazysyncer/lazysyncer.go
+++ backend/labs/lazysyncer/lazysyncer.go
@@ -1,10 +1,13 @@
 package lazysyncer
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 	"path"
+	"sync/atomic"
+	"time"
 
-	"github.com/tidwall/buntdb"
 	"github.com/tidwall/wal"
 )
 
@@ -17,16 +20,96 @@ func NewLazySyncEngine(folder string) (*LazySyncEngine, error) {
 		return nil, err
 	}
 
-	buntdb, err := buntdb.Open(path.Join(folder, "lazy_syncer.db"))
-	if err != nil {
-		return nil, err
-	}
-
-	return &LazySyncEngine{wal: wal, buntdb: buntdb}, nil
+	return &LazySyncEngine{wal: wal}, nil
 
 }
 
 type LazySyncEngine struct {
-	wal    *wal.Log
-	buntdb *buntdb.DB
+	counter atomic.Uint64
+	wal     *wal.Log
+
+	// table__<table_name + row_id> -> wal_index -> [32][8][8]
+	// wal__<wal_index> -> <table_name + row_id> -> [8][32][8]
+}
+
+type Entry struct {
+	Table   string
+	Id      int64
+	GroupOf int64
+}
+
+func (e *LazySyncEngine) Notify(table string, id int64) error {
+	data, err := json.Marshal(Entry{Table: table, Id: id, GroupOf: 0})
+	if err != nil {
+		return fmt.Errorf("json.Marshal: %w", err)
+	}
+
+	e.wal.Write(e.counter.Load(), data)
+	return nil
+}
+
+func (e *LazySyncEngine) BatchNotify(table string, ids []int64) error {
+
+	groupOf := int64(len(ids))
+
+	for _, id := range ids {
+
+		data, err := json.Marshal(Entry{Table: table, Id: id, GroupOf: groupOf})
+		if err != nil {
+			return fmt.Errorf("json.Marshal: %w", err)
+		}
+		e.wal.Write(e.counter.Load(), data)
+
+	}
+
+	return nil
+
+}
+
+func (e *LazySyncEngine) watchWal() error {
+
+	// lastTruncate := uint64(0)
+	lastProcessed := uint64(0)
+
+	for {
+
+		time.Sleep(1 * time.Second)
+
+		lastIndex, err := e.wal.LastIndex()
+		if err != nil {
+			return fmt.Errorf("wal.LastIndex: %w", err)
+		}
+
+		if lastIndex <= lastProcessed {
+			time.Sleep(2 * time.Second)
+			continue
+		}
+
+		i := lastProcessed + 1
+		for i < lastIndex {
+			data, err := e.wal.Read(i)
+			if err != nil {
+				return fmt.Errorf("wal.Read: %w", err)
+			}
+			var entry Entry
+			err = json.Unmarshal(data, &entry)
+			if err != nil {
+				return fmt.Errorf("json.Unmarshal: %w", err)
+			}
+			err = e.processWal(&entry)
+			if err != nil {
+				return fmt.Errorf("processWal: %w", err)
+			}
+
+			lastProcessed = i
+
+		}
+
+	}
+
+}
+
+func (e *LazySyncEngine) processWal(entry *Entry) error {
+
+	return nil
 }
