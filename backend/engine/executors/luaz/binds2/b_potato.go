8cf2d060e095503d2b8935c1b2b88f1d803954e2
bornjre <bornjre@gmail.com>
2026-01-23 03:54:42 +0545

    regester potato module in new bindings

---
+++ backend/engine/executors/luaz/binds2/b_potato.go
@@ -0,0 +1,61 @@
+package binds2
+
+import (
+	"github.com/blue-monads/potatoverse/backend/engine/executors"
+	lua "github.com/yuin/gopher-lua"
+)
+
+const (
+	luaPotatoModuleTypeName = "potato.module"
+)
+
+type luaPotatoModule struct {
+	es     *executors.ExecState
+	submod map[string]map[string]lua.LGFunction
+}
+
+func registerPotatoModuleType(L *lua.LState) {
+	mt := L.NewTypeMetatable(luaPotatoModuleTypeName)
+	L.SetField(mt, "__index", L.NewFunction(potatoModuleIndex))
+}
+
+func potatoModuleIndex(L *lua.LState) int {
+
+	ppmod := L.CheckUserData(1)
+
+	pmod, ok := ppmod.Value.(*luaPotatoModule)
+	if !ok {
+		L.ArgError(1, luaPotatoModuleTypeName+" expected")
+		return 0
+	}
+
+	method := L.CheckString(2)
+
+	table := L.NewTable()
+	L.SetFuncs(table, pmod.submod[method])
+	L.Push(table)
+	return 1
+}
+
+func PotatoModule(es *executors.ExecState, submod map[string]map[string]lua.LGFunction) func(L *lua.LState) int {
+	return func(L *lua.LState) int {
+
+		// Register Potato Module Type
+
+		registerPotatoModuleType(L)
+		ud := L.NewUserData()
+		ud.Value = &luaPotatoModule{
+			es:     es,
+			submod: submod,
+		}
+		L.SetMetatable(ud, L.GetTypeMetatable(luaPotatoModuleTypeName))
+		L.Push(ud)
+
+		// attach exec state in global
+		esud := L.NewUserData()
+		esud.Value = es
+		L.SetGlobal("__es__", esud)
+
+		return 1
+	}
+}
--- backend/engine/executors/luaz/binds2/bind2.go
+++ backend/engine/executors/luaz/binds2/bind2.go
@@ -30,8 +30,23 @@ func GetBindables() map[string]LuaBindable {
 }
 
 func GetExecState(L *lua.LState) *executors.ExecState {
-	execState := L.CheckUserData(1)
-	return execState.Value.(*executors.ExecState)
+	esud := L.GetGlobal("__es__")
+
+	if esud.Type() != lua.LTUserData {
+		panic("__es__ is not a userdata")
+	}
+
+	udata, ok := esud.(*lua.LUserData)
+	if !ok {
+		panic("__es__ is not a userdata")
+	}
+
+	es, ok := udata.Value.(*executors.ExecState)
+	if !ok {
+		panic("__es__ is not an executors.ExecState")
+	}
+
+	return es
 }
 
 const (
