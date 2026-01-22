45718ace38aa103312557ca75ca26041bb179e14
bornjre <bornjre@gmail.com>
2026-01-22 14:32:24 +0545

    new core bindings

---
+++ backend/engine/executors/luaz/binds2/b_core.go
@@ -0,0 +1,196 @@
+package binds2
+
+import (
+	"encoding/json"
+	"errors"
+	"strings"
+
+	"github.com/blue-monads/potatoverse/backend/engine/executors"
+	"github.com/blue-monads/potatoverse/backend/services/datahub"
+	"github.com/blue-monads/potatoverse/backend/services/signer"
+	"github.com/blue-monads/potatoverse/backend/utils/luaplus"
+	"github.com/blue-monads/potatoverse/backend/xtypes"
+	lua "github.com/yuin/gopher-lua"
+)
+
+func CoreBindable(app xtypes.App) map[string]lua.LGFunction {
+
+	engine := app.Engine().(xtypes.Engine)
+	sig := app.Signer()
+	pops := app.Database().GetPackageFileOps()
+
+	return map[string]lua.LGFunction{
+		"publish_event": func(L *lua.LState) int {
+			return corePublishEvent(engine, GetExecState(L), L)
+		},
+		"file_token": func(L *lua.LState) int {
+			return coreSignFsPresignedToken(sig, GetExecState(L), L)
+		},
+		"sign_advisery_token": func(L *lua.LState) int {
+			return coreSignAdviseryToken(sig, GetExecState(L), L)
+		},
+		"parse_advisery_token": func(L *lua.LState) int {
+			return coreParseAdviseryToken(sig, GetExecState(L), L)
+		},
+		"read_package_file": func(L *lua.LState) int {
+			return readPackageFile(pops, GetExecState(L), L)
+		},
+	}
+
+}
+
+type PublishEventOptions struct {
+	Name        string `json:"name"`
+	Payload     any    `json:"payload"`
+	ResourceId  string `json:"resource_id"`
+	CollapseKey string `json:"collapse_key"`
+}
+
+func corePublishEvent(engine xtypes.Engine, es *executors.ExecState, L *lua.LState) int {
+	opts := &PublishEventOptions{}
+	err := luaplus.MapToStruct(L, L.CheckTable(1), opts)
+	if err != nil {
+		L.Push(lua.LString(err.Error()))
+		return 1
+	}
+
+	var payloadBytes []byte
+	if opts.Payload == nil {
+		payloadBytes = []byte{}
+	} else {
+		switch v := opts.Payload.(type) {
+		case string:
+			payloadBytes = []byte(v)
+		case []byte:
+			payloadBytes = v
+		default:
+			// Marshal to JSON for other types (maps, arrays, etc.)
+			jsonData, err := json.Marshal(v)
+			if err != nil {
+				L.Push(lua.LString(err.Error()))
+				return 1
+			}
+			payloadBytes = jsonData
+		}
+	}
+
+	err = engine.PublishEvent(&xtypes.EventOptions{
+		InstallId:   es.InstalledId,
+		Name:        opts.Name,
+		Payload:     payloadBytes,
+		ResourceId:  opts.ResourceId,
+		CollapseKey: opts.CollapseKey,
+		SpaceId:     es.SpaceId,
+	})
+	if err != nil {
+		L.Push(lua.LString(err.Error()))
+		return 1
+	}
+	L.Push(lua.LNil)
+	return 1
+}
+
+type SignFsPresignedTokenOptions struct {
+	Path     string `json:"path"`
+	FileName string `json:"file_name"`
+	UserId   int64  `json:"user_id"`
+}
+
+func coreSignFsPresignedToken(sig *signer.Signer, es *executors.ExecState, L *lua.LState) int {
+	opts := &SignFsPresignedTokenOptions{}
+	err := luaplus.MapToStruct(L, L.CheckTable(1), opts)
+	if err != nil {
+		L.Push(lua.LString(err.Error()))
+		return 1
+	}
+
+	signature, err := sig.SignSpaceFilePresigned(&signer.SpaceFilePresignedClaim{
+		InstallId: es.InstalledId,
+		UserId:    opts.UserId,
+		PathName:  opts.Path,
+		FileName:  opts.FileName,
+	})
+	if err != nil {
+		return luaplus.PushError(L, err)
+	}
+	L.Push(lua.LString(signature))
+	L.Push(lua.LNil)
+	return 2
+}
+
+type SignAdviseryTokenOptions struct {
+	TokenSubType string         `json:"token_sub_type"`
+	UserId       int64          `json:"user_id"`
+	Data         map[string]any `json:"data"`
+}
+
+func coreSignAdviseryToken(sig *signer.Signer, es *executors.ExecState, L *lua.LState) int {
+	opts := &SignAdviseryTokenOptions{}
+	err := luaplus.MapToStruct(L, L.CheckTable(1), opts)
+	if err != nil {
+		L.Push(lua.LString(err.Error()))
+		return 1
+	}
+
+	signature, err := sig.SignSpaceAdvisiery(&signer.SpaceAdvisieryClaim{
+		InstallId:    es.InstalledId,
+		UserId:       opts.UserId,
+		TokenSubType: opts.TokenSubType,
+		Data:         opts.Data,
+		SpaceId:      es.SpaceId,
+	})
+	if err != nil {
+		return luaplus.PushError(L, err)
+	}
+	L.Push(lua.LString(signature))
+	L.Push(lua.LNil)
+	return 2
+}
+
+func coreParseAdviseryToken(sig *signer.Signer, es *executors.ExecState, L *lua.LState) int {
+	token := L.CheckString(1)
+	claim, err := sig.ParseSpaceAdvisiery(token)
+	if err != nil {
+		return luaplus.PushError(L, err)
+	}
+
+	if claim.InstallId != es.InstalledId {
+		return luaplus.PushError(L, errors.New("wrong install id"))
+	}
+
+	if claim.SpaceId != es.SpaceId {
+		return luaplus.PushError(L, errors.New("wrong space id"))
+	}
+
+	resultTable := L.NewTable()
+
+	resultTable.RawSetString("token_sub_type", lua.LString(claim.TokenSubType))
+	resultTable.RawSetString("user_id", lua.LNumber(claim.UserId))
+	resultTable.RawSetString("data", luaplus.MapToTable(L, claim.Data))
+
+	L.Push(resultTable)
+	L.Push(lua.LNil)
+	return 2
+}
+
+func readPackageFile(pops datahub.FileOps, es *executors.ExecState, L *lua.LState) int {
+	fpath := L.CheckString(1)
+
+	fileName := fpath
+	dirPath := ""
+
+	if strings.Contains(fpath, "/") {
+		parts := strings.Split(fpath, "/")
+		fileName = parts[len(parts)-1]
+		dirPath = strings.Join(parts[:len(parts)-1], "/")
+	}
+
+	fileData, err := pops.GetFileContentByPath(es.InstalledId, dirPath, fileName)
+	if err != nil {
+		return luaplus.PushError(L, err)
+	}
+	L.Push(lua.LString(fileData))
+	L.Push(lua.LNil)
+	return 2
+
+}
