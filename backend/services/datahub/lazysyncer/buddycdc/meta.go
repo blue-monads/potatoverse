46ee4face5de6e188ee4eb9fe4bfa9f3654bcefe
bornjre <bornjre@gmail.com>
2026-02-01 04:11:19 +0545

    nit

--- backend/services/datahub/lazysyncer/buddycdc/meta.go
+++ backend/services/datahub/lazysyncer/buddycdc/meta.go
@@ -2,6 +2,7 @@ package buddycdc
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/blue-monads/potatoverse/backend/services/datahub/lazysyncer/lazymodel"
 	"github.com/blue-monads/potatoverse/backend/utils/qq"
@@ -9,6 +10,54 @@ import (
 )
 
 func (b *BuddyCDC) evLoop() {
+
+	for {
+
+		time.Sleep(10 * time.Second)
+
+		currTables, err := b.getMetaForTables()
+		if err != nil {
+			qq.Println("Error fetching buddy meta from local:", err)
+			continue
+		}
+
+		if len(currTables) == 0 {
+			tables, err := b.transport.GetMeta()
+			if err != nil {
+				qq.Println("Error fetching buddy meta:", err)
+				continue
+			}
+
+			err = b.applyTablesMeta(tables)
+			if err != nil {
+				qq.Println("Error applying buddy meta:", err)
+				continue
+			}
+		}
+
+		tables, err := b.transport.GetMeta()
+		if err != nil {
+			qq.Println("Error fetching buddy meta:", err)
+			continue
+		}
+
+		for _, tableMeta := range tables {
+			localMeta, err := b.getMetaForTableId(tableMeta.RemoteTableID)
+			if err != nil {
+				qq.Println("Error fetching buddy meta for table id:", tableMeta.RemoteTableID, err)
+				continue
+			}
+
+			if localMeta.CurrentCDCID < tableMeta.CurrentCDCID {
+				qq.Println("Need to sync table:", localMeta.TableName)
+
+			}
+
+			// localMeta.TableName
+
+		}
+
+	}
 
 }
 
@@ -54,6 +103,23 @@ func (b *BuddyCDC) getMetaForTableId(tableId int64) (*lazymodel.BuddyCDCMeta, er
 	}
 
 	return meta, nil
+}
+
+func (b *BuddyCDC) getMetaForTables() ([]*lazymodel.BuddyCDCMeta, error) {
+
+	metas := []*lazymodel.BuddyCDCMeta{}
+
+	btable := b.buddyMetaTable()
+
+	err := btable.Find(db.Cond{
+		"pubkey": b.buddyPubKey,
+	}).All(&metas)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return metas, nil
 }
 
 func (b *BuddyCDC) getMetaForTable(tableName string) (*lazymodel.BuddyCDCMeta, error) {
