a4e6cd7d84b377a7f4995170ee79370a39de6cc9
bornjre <bornjre@gmail.com>
2026-02-01 02:05:57 +0545

    refactor

--- backend/app/server/rt_buddy/rt_buddy.go
+++ backend/app/server/rt_buddy/rt_buddy.go
@@ -1,12 +1,7 @@
 package rtbuddy
 
 import (
-	"fmt"
-	"net"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"strings"
 	"sync"
 	"time"
 
@@ -69,105 +64,4 @@ func (a *BuddyRouteServer) handleBuddyPing(ctx *gin.Context) {
 		"server_pubkey": serverPubkey,
 	})
 
-}
-
-func (a *BuddyRouteServer) handleBuddyRoute(ctx *gin.Context) {
-	ev, err := verifyNostrAuthCtx(ctx, BuddyAuthExpiry)
-	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-		return
-	}
-
-	turl := ev.Tags[1][1]
-
-	// convert https://example.com/ to http://localhost:3000/
-	// convert zz-12-serverkey.example.com to http://zz-12-serverkey.localhost:3000/
-
-	purl, err := url.Parse(turl)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	host := purl.Host
-
-	newHost := fmt.Sprintf("localhost:%d", a.port)
-
-	if strings.HasPrefix(host, "zz-") {
-		parts := strings.Split(host, ".")
-		suborigin := parts[len(parts)-1]
-		newHost = fmt.Sprintf("%s.localhost:%d", suborigin, a.port)
-	}
-
-	newUrl := url.URL{
-		Scheme:   "http",
-		Host:     newHost,
-		Path:     purl.Path,
-		RawQuery: purl.RawQuery,
-		Fragment: purl.Fragment,
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(&newUrl)
-	proxy.ServeHTTP(ctx.Writer, ctx.Request)
-
-}
-
-func (a *BuddyRouteServer) BuddyAutoRouteMW(ctx *gin.Context) {
-
-	pubkey := a.buddyhub.GetPubkey()
-
-	domainName := ctx.Request.Host
-	if strings.Contains(domainName, ":") {
-		hh, _, err := net.SplitHostPort(ctx.Request.Host)
-		if err != nil {
-			domainName = ctx.Request.Host
-		} else {
-			domainName = hh
-		}
-	}
-
-	qq.Println("@BuddyAutoRouteMW/1", domainName)
-
-	subdomain, err := getSubdomain(domainName)
-	if err != nil {
-		return
-	}
-
-	// current node start
-	if subdomain == "" || subdomain == "main" || subdomain == pubkey {
-		ctx.Next()
-		return
-	}
-
-	if strings.HasPrefix(subdomain, "zz-") && strings.HasSuffix(subdomain, "-main") {
-		ctx.Next()
-		return
-	}
-
-	if strings.HasPrefix(subdomain, "zz-") && strings.HasSuffix(subdomain, a.serverPubKey) {
-		ctx.Next()
-		return
-	}
-
-	// current node end
-
-	// buddy start
-
-	if strings.HasPrefix(subdomain, "buddy") {
-		a.routeToBuddy(subdomain, ctx)
-		return
-	}
-
-	if strings.HasPrefix(subdomain, "zz-") && strings.Contains(subdomain, "buddy") {
-		a.routeToBuddy(subdomain, ctx)
-		return
-	}
-
-	// buddy end
-
-}
-
-func (a *BuddyRouteServer) routeToBuddy(subdomain string, ctx *gin.Context) {
-	extractedPubkey := strings.Split(subdomain, "buddy")[1]
-	a.buddyhub.HandleFunnelRoute(fmt.Sprintf("npub%s", extractedPubkey), ctx)
 }
