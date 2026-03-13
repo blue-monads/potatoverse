cceb03d33eb4ab2103ec7a5d6fcd3d90c1d3df28
bornjre <bornjre@gmail.com>
2026-03-14 05:13:52 +0545

    move things around

---
+++ backend/engine/capabilities/xSystem/xEngine/xLua/xLuaz.go
@@ -0,0 +1,10 @@
+package xlua
+
+/*
+
+run_oneoff_script({script: "print('Hello, World!')"})
+build_vm({script: "print('Hello, World!')"}) -> vm_id
+execute_vm({vm_id: vm_id, method: "hello", params: {name: "World"}}) -> result
+
+
+*/
