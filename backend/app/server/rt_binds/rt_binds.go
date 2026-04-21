40f1205128936b840c2786467fe9a7f0a6920136
bornjre <bornjre@gmail.com>
2026-04-22 04:24:54 +0545

    token decode

--- backend/app/server/rt_binds/rt_binds.go
+++ backend/app/server/rt_binds/rt_binds.go
@@ -2,10 +2,13 @@ package rtbinds
 
 import (
 	"crypto/sha256"
+	"encoding/json"
+	"errors"
 
 	"golang.org/x/crypto/pbkdf2"
 
 	"github.com/blue-monads/potatoverse/backend/engine/hubs/caphub"
+	"github.com/blue-monads/potatoverse/backend/utils/libx/httpx"
 	"github.com/blue-monads/potatoverse/backend/xtypes"
 	"github.com/gin-gonic/gin"
 	"github.com/hako/branca"
@@ -21,7 +24,7 @@ type BindServer struct {
 type HttpBindFn func(ctx *HttpBindContext) (any, error)
 
 func NewBindServer(key []byte) *BindServer {
-	altKey := pbkdf2.Key(key, []byte("UMAMI_POTATO"), 4, 32, sha256.New)
+	altKey := pbkdf2.Key(key, []byte("EXE_UMAMI_POTATO"), 4, 32, sha256.New)
 	return &BindServer{
 		signer: branca.NewBranca(string(altKey)),
 	}
@@ -36,12 +39,47 @@ func (b *BindServer) BindEngine(rg *gin.RouterGroup) {
 
 }
 
+const (
+	XExecHeader = "X-Exec-Header"
+)
+
+type XExecClaim struct {
+	PackageId        int64  `json:"p"`
+	PackageVersionId int64  `json:"v"`
+	SpaceId          int64  `json:"s"`
+	RequestID        string `json:"r"`
+}
+
 func (b *BindServer) authed(h HttpBindFn) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		h(&HttpBindContext{
+
+		token := ctx.GetHeader(XExecHeader)
+		if token == "" {
+			httpx.WriteErr(ctx, errors.New("missing token"))
+			return
+		}
+
+		payload, err := b.signer.DecodeToString(token)
+		if err != nil {
+			httpx.WriteErr(ctx, errors.New("invalid token"))
+			return
+		}
+
+		claim := &XExecClaim{}
+
+		err = json.Unmarshal([]byte(payload), claim)
+
+		if err != nil {
+			httpx.WriteErr(ctx, errors.New("invalid token"))
+			return
+		}
+
+		resp, err := h(&HttpBindContext{
 			Http:           ctx,
-			PackageId:      0,
-			PackageVersion: 0,
+			PackageId:      claim.PackageId,
+			PackageVersion: claim.PackageVersionId,
 		})
+
+		httpx.WriteJSON(ctx, resp, err)
 	}
 }
